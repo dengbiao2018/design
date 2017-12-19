5d93f92158b5e2c71a803afd2055574b868dee94
dengbiao2017 <dengbiao717>
2017-12-19 15:52:38 +0800

    secend website

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // JavaScript Document
 $(function(){
-
-
+	var personal_item = $('.personal .img-a,.personal h2 a');
+	personal_item.click(function(){
+		$(".MediaModal").show();
+		$('body').addClass("overflow-hidden");
+	})
+	$(".modalClose,.MediaImage").click(function(){
+		$(".MediaModal").hide();
+		$('body').removeClass("overflow-hidden");
+	})
 });
